34b47a366828701c97ecbc90643c2cd624a2ce32
6GGHecker9 <129679302+6GGHecker9@users.noreply.github.com>
2023-04-02 19:16:02 +0200

    Update GG Hecks.lua

--- GG Hecks.lua
+++ GG Hecks.lua
@@ -33,34 +33,12 @@ local Movement = Window:NewTab("Movement")
 local UnfairMovement = Movement:NewSection("Unfair Movement")
 
 UnfairMovement:NewButton("Fly", "Lets you fly", function()
-  local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/6GGHecker9/Roblox-Scripts/main/Fly.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/6GGHecker9/Roblox-Scripts/main/Fly.lua"))()
 end)
 
 
 UnfairMovement:NewButton("Left ctrl + click to teleport", "Teleport where you want! [NOT FOR MOBILE]", function()
-local UIS = game:GetService("UserInputService")
-
-local Player = game.Players.LocalPlayer
-local Mouse = Player:GetMouse()
-
-
-function GetCharacter()
-   return game.Players.LocalPlayer.Character
-end
-
-function Teleport(pos)
-   local Char = GetCharacter()
-   if Char then
-       Char:MoveTo(pos)
-   end
-end
-
-
-UIS.InputBegan:Connect(function(input)
-   if input.UserInputType == Enum.UserInputType.MouseButton1 and UIS:IsKeyDown(Enum.KeyCode.LeftControl) then
-       Teleport(Mouse.Hit.p)
-   end
-end)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/6GGHecker9/Roblox-Scripts/main/CTT.lua"))()
 end)
 
 
