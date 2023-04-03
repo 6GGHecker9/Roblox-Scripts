b1beb973fc098fa2251fad133b5f1c3c35fd3e8c
6GGHecker9 <129679302+6GGHecker9@users.noreply.github.com>
2023-04-03 14:40:18 +0200

    Update GG Hecks.lua

--- GG Hecks.lua
+++ GG Hecks.lua
@@ -74,34 +74,8 @@ local Troll = Window:NewTab("Troll")
 local Fling = Troll:NewSection("Fling")
 
 Fling:NewButton("Fling", "Fling people!", function()
--- Settings
-local Settings = {
-   Target = "kaleguy123456" -- Target Name (Not display name)
-}
 
--- Objects
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-
-local LocalPlayer = Players.LocalPlayer
-local Target = Players:FindFirstChild(Settings.Target)
-
-local BodyAngularVelocity = Instance.new("BodyAngularVelocity")
-BodyAngularVelocity.AngularVelocity = Vector3.new(10^6, 10^6, 10^6)
-BodyAngularVelocity.MaxTorque = Vector3.new(10^6, 10^6, 10^6)
-BodyAngularVelocity.P = 10^6
-
--- Start
-if not Target then return end
-BodyAngularVelocity.Parent = LocalPlayer.Character.HumanoidRootPart
-
-while Target.Character.HumanoidRootPart and LocalPlayer.Character.HumanoidRootPart do
-   RunService.RenderStepped:Wait()
-   LocalPlayer.Character.HumanoidRootPart.CFrame = Target.Character.HumanoidRootPart.CFrame * LocalPlayer.Character.HumanoidRootPart.CFrame.Rotation
-   LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new()
-end
-   
-BodyAngularVelocity.Parent = nil
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/6GGHecker9/Roblox-Scripts/main/Fling.lua"))()
 
 end)
 
