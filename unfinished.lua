ed0f7b1167ef93bf5263ae53db958b8e32d278e4
6GGHecker9 <129679302+6GGHecker9@users.noreply.github.com>
2024-02-16 21:17:01 +0100

    it bc it gon loop and nevr start whole tim unfinished.lua

--- unfinished.lua
+++ unfinished.lua
@@ -2049,7 +2049,7 @@ HotkeyTab.MouseButton1Click:Connect(function()
 	ScrollingFrame.CanvasPostion = Vector2.new(0,532.5)
 end)
 
-local function changeCam()
+--[[local function changeCam()
 	while true do
 		if CDB == false then
 			cam.CameraType = "Custom"
@@ -2058,7 +2058,7 @@ local function changeCam()
 	end
 end
 
-changeCam()
+changeCam()]]
 
 --IntroAnim Play
 task.wait(2)
