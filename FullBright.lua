0bc1a0765e7af43dede420d65b6cfecff2a25945
6GGHecker9 <129679302+6GGHecker9@users.noreply.github.com>
2024-02-17 12:30:49 +0100

    Create FullBright.lua

---
+++ FullBright.lua
@@ -0,0 +1,10 @@
+local part = Instance.new("Part")
+local light = Instance.new("PointLight")
+
+part.Transparency = 1
+light.Parent = part
+light.Brightness = 30
+
+while true do
+  part.CFrame = game.Workspace.CurrentCamera.CFrame
+end
